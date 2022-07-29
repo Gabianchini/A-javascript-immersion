8c55d75cfa9d4944d109c511b21213cdab237300
Gabianchini <gabibisdx@gmail.com>
2022-07-29 17:28:46 -0300

    added session about destructuring arrays

---
+++ destructuring-arrays/script.js
@@ -0,0 +1,7 @@
+//Desestruturação em arrays
+//podemos definir variaveis com os valores de um array usando o destructuring
+//ex:
+
+let nomes = ['Matheus', 'João', 'Michael']; //cria array
+let [nomeA, nomeB, nomeC] = nomes; //define variaveis pros elementos(levando em conta os indices) do array
+console.log(nomeA, nomeB, nomeC);
