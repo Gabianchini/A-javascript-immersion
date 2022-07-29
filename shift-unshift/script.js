d4ccbc790bd1a70ccb5a80c58f79a60394ecb8ef
Gabianchini <gabibisdx@gmail.com>
2022-07-29 15:27:26 -0300

    added session about shift and unshift

---
+++ shift-unshift/script.js
@@ -0,0 +1,14 @@
+//Métodos de array shift e unshift
+//shift: Remove o primeiro elemento de um array
+//unshift: Adiciona um ou mais elementos no inicio de um array
+
+//exemplo shift:
+let frutas = ["maçã", "melao", "uva"];
+let frutaRemovida = frutas.shift();
+console.log(frutas);
+console.log(frutaRemovida);
+
+//exemplo unshift
+
+frutas.unshift("Melancia");
+console.log(frutas);
